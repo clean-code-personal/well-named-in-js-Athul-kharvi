699ad996092f1cd80d3c9065e46d800e2becedcd
Athul kharvi <98105204+Athul-kharvi@users.noreply.github.com>
2025-01-15 22:40:51 +0530

    Update ReferenceManual.js

--- app/ReferenceManual.js
+++ app/ReferenceManual.js
@@ -1,16 +1,23 @@
-const { ColorCoder, MajorColorNames, MinorColorNames } = require('./ColorCoder');
+const { ColorCoder } = require('./ColorCoder');
 const { getColorFromPairNumber } = require('./ColorMapping');
 
 function generateReferenceManual() {
-    const totalPairs = MajorColorNames.length * MinorColorNames.length;
+    const totalPairs = ColorCoder.MajorColorNames.length * ColorCoder.MinorColorNames.length;
     let manual = "Color Code Reference Manual:\n";
+    manual += "Pair Number\tMajor Color\tMinor Color\n";
+    manual += "-------------------------------------\n";
 
     for (let i = 1; i <= totalPairs; i++) {
         const pair = getColorFromPairNumber(i);
-        manual += `Pair ${i}: ${pair.toString()}\n`;
+        manual += `${i}\t\t${pair.majorColor}\t\t${pair.minorColor}\n`;
     }
 
     return manual;
 }
 
-module.exports = { generateReferenceManual };
+function printReferenceManual() {
+    const manual = generateReferenceManual();
+    console.log(manual);
+}
+
+module.exports = { generateReferenceManual, printReferenceManual };
